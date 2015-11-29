8553586dde3a5d290fbb0895ee9987e3a2d998cb
Hugo Argueta <hugoargueta94@gmail.com>
2015-11-28 17:36:46 -0800

    added instruction processing example

--- testing.py
+++ testing.py
@@ -1,11 +1,17 @@
+def add(inst):
+    print(int(inst[1]) + int(inst[2]))
 
-
-
-
-def main():
+def subt(inst):
     print("Hello, world")
 
+def main():
+    instruction = raw_input("Enter instruction: \n")
 
+    print(instruction.split())    
 
+    inst_list = instruction.split()
+
+    if inst_list[0] == "ADD":
+        add(inst_list)
 
 main()
