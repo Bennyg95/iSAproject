7ea99507ce6cf7b28249d46ceb91e1b15a1f6ccf
Joseph <josephmolina3388@gmail.com>
2015-11-30 16:44:19 -0800

    Added sub and mult function

--- testing.py
+++ testing.py
@@ -2,7 +2,13 @@ def add(inst):
     print(int(inst[1]) + int(inst[2]))
 
 def subt(inst):
-    print("Hello, world")
+    print(int(inst[1]) - int(inst[2]))
+
+def mult(inst):
+    print(int(inst[1]) * int(inst[2]))
+
+def div(inst):
+    print(float(inst[1]) / float(inst[2]))
 
 def main():
     instruction = raw_input("Enter instruction: \n")
@@ -14,4 +20,15 @@ def main():
     if inst_list[0] == "ADD":
         add(inst_list)
 
+    elif inst_list[0] == "SUBT":
+	 subt(inst_list)
+
+    elif inst_list[0] == "MULT":
+	 mult(inst_list)
+
+    elif inst_list[0] == "DIV":
+	 div(inst_list)
+	
 main()
+
+
