509fc0738c5ed1037e48ee45b6b2cd331602a5ee
Benito <sanchez.benitog95@hotmail.com>
2015-12-12 21:37:58 +0000

    updating all files.

--- README.md
+++ README.md
@@ -1,37 +1,91 @@
 # iSAproject
 
+### Description
 
     In this project we will be designing our own instruction set architecture.
     Our choice of byte-organization is big endian, due to the fact that it would be easier to understand and easier to debug.
     Our ISA also contains an accumulator.
-    List of possible operations:
-        Input
-        Output
-        Halt
-        fallas()
-        ADD
-        Subtract
-        Multiply
-        Divide
-        STORE
-        ACC
-        LOAD
-        LOADI
-        STOREI
+    
+### List of possible operations:
+    
+    INPUT
+        * This will input a value into the accumulator.
+        
+    OUTPUT
+        * Will ouput a value 
+        
+    HALT
+        * Stops the program
+        
+    func functionName :
+    
+    end
+    
+        * this is the syntax for creating a function. Remember to state 'func'
+            because that is the key that initializes the function. Instead of 'functionName
+            state your own function name. NOTE: don't forget to add a space after 
+            'functionName' otherwise you will get a syntax error. The 'end' keyword
+            states the end of the function.
+            
+    
+    ADD 
+        * This will add two values together and put the sum in the accumulator.
+        Ex: 
+            ADD val1 val2
+            
+        NOTE: This operation would substitute anything in the accumulator to the new result.
+        
+    SUBT
+        * Subtracts two values and puts the result in the accumulator.
+        
+            SUBT val1 val2
+            
+        NOTE: This operation would substitute anything in the accumulator to the new result.
+    
+    MULT
+        * Multiplies two values together and puts the result in the accumulator.
+        
+            MULT val1 val2
+            
+        NOTE: This operation would substitute anything in the accumulator to the new result
+    
+    DIV
+        * Divides two values and the result is inserted into the accumalator.
+            
+            DIV val1 val2
+        
+        NOTE: This operation would substitute anything in the accumalator to the result.
+        
+    STORE
+        * Will substitute the value in the current value in the accumalator with the value given.
+            Example: STORE x
+        
+    LOAD
+        * Will load the given variable into the accumalator.
+        
+            Example : LOAD var1
+        
+        NOTE: This will replace anything in the accumalator with the loaded value
+        
+    STOREI
         <, > , =, !=
+        
         Loop(start : end)
+        
         ARRAY:
-            NA.add(value)
-            .at(location)
-            .size()
-            .replace(place : value)
-            .remove(location)
+            ARRAY_NAME.add(value)
+            ARRAY_NAME.at(location)
+            ARRAY_NAME.size()
+            ARRAY_NAME.replace(place : value)
+            ARRAY_NAME.remove(location)
 
 #### Instructions
 
+    variables and functions should be declared before the main function.
+
 Editing text file
     
-    The code has to be written in the the program.py file. If you need to reference a file take a looka t TestingFile.txt.
+    The code has to be written in the the program.py file. If you need to reference a file take a look at TestingFile.txt.
     In order to fun you're code type in the following command on a terminal:
         
         python isAProject.py
--- isAProject.py
+++ isAProject.py
@@ -1,45 +1,59 @@
 #Global variable names
 register = dict()
 accumulator = 0
-opcode_list = ["ADD", "SUBT", "MULT", "DIV", "LOAD", "OUTPUT", "INPUT"]
+opcode_list = ["ADD", "SUBT", "MULT", "DIV", "LOAD", "OUTPUT", "INPUT", "STORE"]
 register = dict()
 
-#def add(inst):
-    #print(int(inst[1]) + int(inst[2]))
+def addToArray():
+    pass
     
 def addToRegister(inst):
+    varName = inst[1]
+    varValue = inst[3]
     
-    register[inst[1]]
-    
+    try:
+        register[varName] = int(varValue)
+    except:
+        register[varName] = varValue
+        
 def loadVar(inst):
     varKey = inst[1]
     
-    register["var1"] = 2
-    
-    if inst[1] in register.keys():
-        accumulator = register[inst[1]]
-        print accumulator
+    if varKey in register.keys():
+        accumulator = register[varKey]
+        print "LOAD: ", accumulator
     else:
         print "FALLAS Variable " + varKey + " was not declared."
+        
+def storeVal(inst):
+    varName = inst[1]
+    
+    register[varName] = accumulator
 
 # This function will output a variable 
 # OUTPUT var
 def outputVar(inst):
-    print(inst[1])
-
+    varName = inst[1]
+    varVal = None
+    
+    if varName in register.keys():
+        varVal = register[varName]
+        print "OUTPUT: ", varVal
+    else:
+        print "FALLAS: Variable " + varName + " was not declared."
+    
 ## INPUT funcion
 # # INPUT var
-#def inputVar(inst):
+def inputVar(inst):
     # where would we be saving the inputed variable?
-
-def subt(inst):
-    print(int(inst[1]) - int(inst[2]))
-
-def mult(inst):
-    print(int(inst[1]) * int(inst[2]))
-
-def div(inst):
-    print(float(inst[1]) / float(inst[2]))
+    userVal = raw_input()
+    varName = inst[1]
+    
+    try:
+        register[varName] = int(userVal)
+    except:
+        register[varName] = userVal
+    
 
 # Checking the format of the opcodes
 def checkInst(inst):
@@ -49,25 +63,113 @@ def checkInst(inst):
         if opcode in opcode_list:
             if opcode == "LOAD":
                 loadVar(inst)
-            if opcode == "OUTPUT":
+            elif opcode == "OUTPUT":
                 outputVar(inst)
-           # if opcode == "INPUT":
-                #inputVar(inst)
+            elif opcode == "INPUT":
+                inputVar(inst)
+            elif opcode == "STORE":
+                storeVal(inst)
     
     elif len(inst) == 3:
         if opcode in opcode_list:
             if opcode == "ADD":
                 add(inst)
+            elif opcode == "SUBT":
+                subt(inst)
+            elif opcode == "MULT":
+                mult(inst)
+            elif opcode == "DIV":
+                div(inst)
            
     elif len(inst) == 4:
-        pass
+        if (inst[0] == "Str" or inst[0] == "Dec") and inst[2] == "=":
+            addToRegister(inst)
+        else:
+            print "FALLAS: Invalid declaration."
     
-        
-        
-
+    
 #Addition function
 #Takes in a list which is an instruction then determines if the left and right operations are variables or values.
 def add(inst):
+    left_num = 0
+    right_num = 0
+    
+    try:
+        left_num = int(inst[1])
+    except:
+        if inst[1] in register.keys():
+            left_num = register(inst[1])
+        else:
+            var1 = inst[1]
+            print "FALLAS: Variable " + var1 + " was not declared."
+        
+    try:
+        right_num = int(inst[2])
+    except:
+        if inst[2] in register.keys():
+            right_num = register[inst[2]]
+        else:
+            var2 = inst[2]
+            print "FALLAS: Variable " + var2 + " was not declared."
+            
+    accumulator = left_num + right_num
+    
+    print accumulator
+    
+def subt(inst):
+    left_num = 0
+    right_num = 0
+    
+    try:
+        left_num = int(inst[1])
+    except:
+        if inst[1] in register.keys():
+            left_num = register(inst[1])
+        else:
+            var1 = inst[1]
+            print "FALLAS: Variable " + var1 + " was not declared."
+            
+    try:
+        right_num = int(inst[2])
+    except:
+        if inst[2] in register.keys():
+            right_num = register[inst[2]]
+        else:
+            var2 = inst[2]
+            print "FALLAS: Variable " + var2 + " was not declared."
+            
+    accumulator =  left_num - right_num
+    
+    print accumulator
+        
+            
+def mult(inst):
+    left_num = 0
+    right_num = 0
+    
+    try:
+        left_num = int(inst[1])
+    except:
+        if inst[1] in register.keys():
+            left_num = register(inst[1])
+        else:
+            var1 = inst[1]
+            print "FALLAS: Variable " + var1 + " was not declared."
+            
+    try:
+        right_num = int(inst[2])
+    except:
+        if inst[2] in register.keys():
+            right_num = register(inst[2])
+        else:
+            var2 = inst[2]
+            print "FALLAS: Variable " + var2 + " was not declared."
+            
+    accumulator = left_num * right_num
+    
+    print accumulator
+    
+def div(inst):
     left_num = 0;
     right_num = 0;
     
@@ -78,7 +180,7 @@ def add(inst):
             left_num = register(inst[1])
         else:
             var1 = inst[1]
-            print "FALLAS: Variable " + var1 + "was not declared."
+            print "FALLAS: Variable " + var1 + "vwas not declared."
         
     try:
         right_num = int(inst[2])
@@ -87,27 +189,29 @@ def add(inst):
             right_num = register[inst[2]]
         else:
             var2 = inst[2]
-            print "FALLAS: Variable " + var2 + "was not declared."
+            print "FALLAS: Variable " + var2 + " was not declared."
             
-    accumalator = left_num + right_num
-    
-    print accumalator
+    if right_num != 0:
+        accumulator = left_num / right_num
+        print accumulator
+    else:
+        print "FALLAS: Cannot divide by 0."
+        
 
 #Main function of the program
 def main():
-    
     bencounter = 0 
-    print "Welcome!"
+    print "Welcome! to the world's most amazing ISA"
     
     fin = open("TestingFile.txt", "r")
     
+
     for line in fin.readlines():
         inst = line.split()
         
         if len(inst) == 0:
             continue
-        
+    
         checkInst(inst)
+        
 main()
-
-
